3bc094f8235d7132f6fa8e7aaf247b3784f0b530
vaibhav47sharma <vs3br@virginia.edu>
2018-09-22 15:32:07 -0400

    little cleaning of the post data

--- data_processing/data_processing_regression.R
+++ data_processing/data_processing_regression.R
@@ -1,3 +1,29 @@
 #setwd('C:\\Users\\vaibhav\\Documents\\UVA\\Fall\\Capstone\\Code\\ARL\\data_processing')
 
-pre_data <- read.csv('dataframes\\pre_data.csv')
+post.data <- read.csv('dataframes\\post_data.csv')
+post.rating.data <- data.frame()
+post.test.data <- data.frame()
+
+vector.is.numeric <- function(x) {
+  #Function that gives true if there are only numeric values
+  return(!anyNA(as.numeric(as.character(x))))
+}
+
+for (i in 1: 18) {
+  if (i %% 2 == 1) {
+    post.rating.data <- rbind(post.rating.data, as.data.frame(post.data[i,]))
+  } else {
+    post.test.data <- rbind(post.test.data, as.data.frame(post.data[i,]))
+  }
+}
+
+#Finding which columns are numeric
+nums <- unlist(lapply(post.test.data, vector.is.numeric)) 
+nums <- names(which(nums == TRUE))
+
+#Removing all the numeric columns from the test data
+`%ni%` <- Negate(`%in%`)
+post.clean.test.data <- subset(post.test.data, select = names(post.test.data) %ni% nums)
+
+#Removing all the columns from the rating data that were removed from the test data
+post.clean.rating.data <- subset(post.rating.data, select = names(post.rating.data) %ni% nums)
