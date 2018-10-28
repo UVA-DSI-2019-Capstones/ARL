03aff1756837e5e52836b795003ce763d51589d0
Beni Shpringer <bshpringer@gmail.com>
2018-10-28 16:18:43 -0400

    simplify setup script

--- db/setup.py
+++ db/setup.py
@@ -5,15 +5,11 @@ def create_connection(db_file):
     try:
         conn = sqlite3.connect(db_file)
         print(sqlite3.version)
+        return(conn)
     except Error as e:
         print(e)
-    finally:
-        conn.close()
- 
-if __name__ == '__main__':
-    create_connection("database.db")
 
-db = sqlite3.connect("database.db")
+db = create_connection("database.db")
 
 create_avatar_prompt = """
 CREATE TABLE IF NOT EXISTS avatar_prompt (
@@ -47,6 +43,7 @@ CREATE TABLE IF NOT EXISTS turk_response (
  trainee_response_id int NOT NULL,
  turk_id text NOT NULL,
  turk_response_text text NOT NULL,
+ prompt_type text NOT NULL,
  comment text,
  FOREIGN KEY(trainee_response_id) REFERENCES trainee_response(id)
 );
@@ -56,3 +53,4 @@ db.execute(create_avatar_prompt)
 db.execute(create_trainee_response)
 db.execute(create_turk_response)
 db.commit()
+db.close()
