0ec043bf8ce617f7891407c04a4f3184996e6305
vaibhav47sharma <vs3br@virginia.edu>
2018-11-04 10:42:27 -0500

    database creation query updated

--- db/setup.py
+++ db/setup.py
@@ -13,39 +13,42 @@ db = create_connection("database.db")
 
 create_avatar_prompt = """
 CREATE TABLE IF NOT EXISTS avatar_prompt (
- id integer PRIMARY KEY AUTO_INCREMENT,
+ id integer AUTO_INCREMENT,
  avatar_name text NOT NULL,
  experiment text NOT NULL,
  sub_section integer NOT NULL,
  identifier text NOT NULL,
  avatar_prompt_text text NOT NULL,
  avatar_culture text,
- comment text
+ comment text,
+ primary key (id)
 );
 """
 
 create_trainee_response = """
 CREATE TABLE IF NOT EXISTS trainee_response (
- id integer PRIMARY KEY AUTO_INCREMENT,
+ id integer AUTO_INCREMENT,
  avatar_prompt_id int NOT NULL,
  identifier text NOT NULL,
  response_text text NOT NULL,
  response_score real NOT NULL,
  response_feedback text NOT NULL,
  comment text,
- FOREIGN KEY(avatar_prompt_id) REFERENCES avatar_prompt(id)
+ FOREIGN KEY(avatar_prompt_id) REFERENCES avatar_prompt(id),
+ primary key (id)
 );
 """
 
 create_turk_response = """
 CREATE TABLE IF NOT EXISTS turk_response (
- id integer PRIMARY KEY AUTO_INCREMENT,
+ id integer AUTO_INCREMENT,
  trainee_response_id int NOT NULL,
  turk_id text NOT NULL,
  turk_response_text text NOT NULL,
  prompt_type text NOT NULL,
  comment text,
- FOREIGN KEY(trainee_response_id) REFERENCES trainee_response(id)
+ FOREIGN KEY(trainee_response_id) REFERENCES trainee_response(id),
+ primary key (id)
 );
 """
 
