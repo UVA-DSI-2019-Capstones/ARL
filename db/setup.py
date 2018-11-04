f81a8d0f757b50c99f61718addb8a398ef305180
Beni Shpringer <bshpringer@gmail.com>
2018-11-04 11:32:49 -0500

    remove autoincrement

--- db/setup.py
+++ db/setup.py
@@ -13,42 +13,39 @@ db = create_connection("database.db")
 
 create_avatar_prompt = """
 CREATE TABLE IF NOT EXISTS avatar_prompt (
- id integer AUTO_INCREMENT,
+ id integer PRIMARY KEY,
  avatar_name text NOT NULL,
  experiment text NOT NULL,
  sub_section integer NOT NULL,
  identifier text NOT NULL,
  avatar_prompt_text text NOT NULL,
  avatar_culture text,
- comment text,
- primary key (id)
+ comment text
 );
 """
 
 create_trainee_response = """
 CREATE TABLE IF NOT EXISTS trainee_response (
- id integer AUTO_INCREMENT,
+ id integer PRIMARY KEY,
  avatar_prompt_id int NOT NULL,
  identifier text NOT NULL,
  response_text text NOT NULL,
  response_score real NOT NULL,
  response_feedback text NOT NULL,
  comment text,
- FOREIGN KEY(avatar_prompt_id) REFERENCES avatar_prompt(id),
- primary key (id)
+ FOREIGN KEY(avatar_prompt_id) REFERENCES avatar_prompt(id)
 );
 """
 
 create_turk_response = """
 CREATE TABLE IF NOT EXISTS turk_response (
- id integer AUTO_INCREMENT,
+ id integer PRIMARY KEY,
  trainee_response_id int NOT NULL,
  turk_id text NOT NULL,
  turk_response_text text NOT NULL,
  prompt_type text NOT NULL,
  comment text,
- FOREIGN KEY(trainee_response_id) REFERENCES trainee_response(id),
- primary key (id)
+ FOREIGN KEY(trainee_response_id) REFERENCES trainee_response(id)
 );
 """
 
