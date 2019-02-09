8799bd6293c2c9d2543ee956459dbd41cd7fbf3f
martinbolger <39777263+martinbolger@users.noreply.github.com>
2019-02-09 10:52:22 -0500

    LDA

--- db/LDA.py
+++ db/LDA.py
@@ -1,24 +1,9 @@
-import shorttext
-import sqlite3
 import pandas as pd
-import os.path
+import os
 
-conn = sqlite3.connect("database.db")
-# df = pd.read_sql_query("select * from trainee_response;", conn)
-df = pd.read_sql_query("SELECT * FROM trainee_response INNER JOIN turk_response ON trainee_response.identifier = turk_response.identifier;", conn)
+train = pd.read_csv('train_set.csv')
+# df_test = pd.DataFrame('test_set.csv')
 
-# def score_round(score):
-#     if score < 2.3:
-#         return 1
-#     elif score >= 2.3 and score < 4:
-#         return 3
-#     return 5
-
-# df['response_round_score'] = df['response_score'].apply(score_round)
-
-
-train = df.sample(frac=1, random_state=200)
-test = df.drop(train.index)
 
 #Preprocessing
 
@@ -29,6 +14,7 @@ train['processed'] = train['response_text'].apply(pre)
 
 corpus = train['processed'].apply(lambda x : x.split(' '))
 index = list(corpus.index)
+print(corpus)
 
 #Package to pre process
 import gensim
@@ -41,11 +27,12 @@ dictionary = gensim.corpora.Dictionary(corpus)
 bow_corpus = [dictionary.doc2bow(doc) for doc in corpus]
 
 numpy.random.seed(1)
-upper_bound = 30
+upper_bound = 31
 for i in range(2,upper_bound):
 	if not(os.path.isfile('LDA_{}_topics.csv'.format(i))):
 		lower_bound = i
 		break
+
 
 for number_of_topics in range(lower_bound,upper_bound):
 	model = ldamodel.LdaModel(bow_corpus, id2word=dictionary, num_topics=number_of_topics)
@@ -59,7 +46,7 @@ for number_of_topics in range(lower_bound,upper_bound):
 
 
 	for i, item in enumerate(bow_corpus):
-		text = [str(df.loc[index[i], "response_text"])]
+		text = [str(train.loc[i, "response_text"])]
 		doc_type, percent = map(list, zip(*model.get_document_topics(item, minimum_probability=0.000001)))
 		print(model.get_document_topics(item, minimum_probability=0.000001))
 		row = text + percent
@@ -73,7 +60,7 @@ for number_of_topics in range(lower_bound,upper_bound):
 
 	lda_df.to_csv('LDA_{}_topics.csv'.format(number_of_topics))
 	print('Loop for {} topics complete'.format(number_of_topics))
-	lda_df.drop(df.index, inplace=True)
+	lda_df.drop(train.index, inplace=True)
 
 
     
