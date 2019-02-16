e552babcd7ccdac841d5be75de24255d2a648dcd
vaibhav47sharma <vs3br@virginia.edu>
2019-02-16 17:34:12 -0500

    word2vec for logistic regression

--- db/word2vec_modeling.py
+++ db/word2vec_modeling.py
@@ -2,17 +2,26 @@ import pandas as pd
 import os
 import glob
 from sklearn.linear_model import LogisticRegressionCV
-os.chdir('word2vecmodels')
+from sklearn.metrics import f1_score
+os.chdir(os.path.join(os.getcwd(),'db','word2vecmodels'))
 
 train_files = [i for i in glob.glob('*train*.{}'.format('csv'))]
 test_files = [i for i in glob.glob('*test*.{}'.format('csv'))]
 
 train_test_list = list(zip(train_files, test_files))
 
-for train_df, test_df in train_test_list:
-  clf = LogisticRegressionCV(cv=10, random_state=0, solver='lbfgs', multi_class='multinomial').fit(train_df, train[
-    'response_round_score'])
+for train, test in train_test_list:
+  train_df = pd.read_csv(train)
+  test_df = pd.read_csv(test)
 
-  clf.predict(test_df)
-  score = clf.score(test_df, test['response_round_score'])
-  print('For {} topics, score is {}'.format(number_of_topics, score))
+  x_train = train_df.iloc[:,1:-1]
+  y_train = train_df['response_round_score']
+
+  x_test = test_df.iloc[:, 1:-1]
+  y_test = test_df['response_round_score']
+
+  clf = LogisticRegressionCV(cv=10, random_state=0, solver='lbfgs', multi_class='multinomial').fit(x_train,
+                                                                                                   y_train)
+  score = clf.score(x_test, y_test)
+  print(str(''.join([n for n in train if n.isdigit()][1:]) + ' ' + str(score) + ' ' +
+            str(f1_score(y_test, clf.predict(x_test), average = 'weighted'))))
