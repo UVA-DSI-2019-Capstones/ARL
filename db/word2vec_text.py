7e481fe88a606565a6871b223a3e75194c94c4ce
vaibhav47sharma <vs3br@virginia.edu>
2019-02-16 15:54:34 -0500

    created word2vec models for 100 to 500 dimensions, 50 steps

---
+++ db/word2vec_text.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import os
+os.chdir('db')
+from pathlib import Path
+import sys
+#Package to pre process
+import gensim
+from gensim.utils import simple_preprocess
+from gensim.models import ldamodel
+from gensim.test.utils import datapath
+import numpy as np
+from gensim.models import Word2Vec
+from shorttext.utils import standard_text_preprocessor_1
+pre = standard_text_preprocessor_1()
+train = pd.read_csv('train_set.csv')
+test = pd.read_csv('test_set.csv')
+
+# df_test = pd.DataFrame('test_set.csv')
+
+def get_process_data_frame(data_frame):
+  # Preprocessing
+  data_frame['processed'] = data_frame['response_text'].apply(pre)
+  corpus = data_frame['processed'].apply(lambda x: x.split(' '))
+  return corpus
+
+
+train_corpus = get_process_data_frame(train)
+test_corpus = get_process_data_frame(test)
+
+def get_word_2_vec_df(corpus, dim = 100):
+  # train word2vec model
+  # Default dimensions is 100
+  # sg: (default 0 or CBOW) The training algorithm, either CBOW (0) or skip gram (1).
+  model = Word2Vec(corpus, min_count=1)
+  # summarize the loaded model
+  # print(model)
+
+  # summarize vocabulary
+  # words = list(model.wv.vocab)
+  # print(words)
+
+  # access vector for one word
+  # print(model['think'])
+
+  return pd.DataFrame(corpus.apply(lambda x: np.mean(model[x], axis=0)))
+
+dir = os.getcwd()
+for dim in range(100, 501, 50):
+  train_set = get_word_2_vec_df(train_corpus, dim)
+  test_set = get_word_2_vec_df(test_corpus, dim)
+
+  train_file = "word_2_vec_train{}.csv".format(dim)
+  test_file = "word_2_vec_test{}.csv".format(dim)
+
+  train_set.to_csv(os.path.join(dir, 'word2vecmodels', train_file))
+  test_set.to_csv(os.path.join(dir, 'word2vecmodels', test_file))
+
+
